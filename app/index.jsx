ee444acceee9e9938127949e5bce65cf77959965
Moazam05 <salmanmoazam08@gmail.com>
2024-08-17 23:04:34 +0500

    fonts loaded

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello World</Text>
+      <Text className="text-3xl font-pblack">Hello World</Text>
       <Text>This is the first page of your app.</Text>
     </View>
   );
