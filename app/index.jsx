d3360c1b3127dbcbf987b8b4cdeb8f490df303cc
Moazam05 <salmanmoazam08@gmail.com>
2024-08-18 14:18:17 +0500

    rename files

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Hello World</Text>
-      <Text>This is the first page of your app.</Text>
+      <Text className="text-3xl font-pblack">Aora</Text>
+      <Link href="/home" className="text-blue-500">
+        Go to Home
+      </Link>
     </View>
   );
 }
