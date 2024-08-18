1d07dd6d490f18223bc7f78eea1f6559caccd10b
Moazam05 <salmanmoazam08@gmail.com>
2024-08-18 19:33:08 +0500

    Sign up screen

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,80 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, SafeAreaView, ScrollView, Image, Text } from "react-native";
+import { useState } from "react";
+
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignUp = () => {};
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="w-full flex-1 justify-center px-4">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white font-semibold mt-10 font-psemibold">
+            Sign Up to Aora
+          </Text>
+
+          <FormField
+            title="Username"
+            placeholder="Enter your username"
+            value={form.username}
+            handleChangeText={(value) => setForm({ ...form, username: value })}
+            otherStyles="mt-10"
+          />
+
+          <FormField
+            title="Email"
+            placeholder="Enter your email"
+            value={form.email}
+            handleChangeText={(value) => setForm({ ...form, email: value })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            placeholder="Enter your password"
+            value={form.password}
+            handleChangeText={(value) => setForm({ ...form, password: value })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={handleSignUp}
+            containerStyles="mt-7"
+            isLoading={isLoading}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
