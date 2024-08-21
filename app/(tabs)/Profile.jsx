b423310a01ad095125fd3141f8eaa910daf61554
Moazam05 <salmanmoazam08@gmail.com>
2024-08-21 20:12:48 +0500

    Profile UI

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -7,6 +7,7 @@ import VideoCard from "../../views/Home/components/VideoCard";
 import { getUserPosts } from "../../lib/appwrite";
 import { useGlobalContext } from "../../context/GlobalProvider";
 import { icons } from "../../constants";
+import InfoBox from "../../components/InfoBox";
 
 const Profile = () => {
   const { user, setUser, setIsLogged } = useGlobalContext();
@@ -30,6 +31,35 @@ const Profile = () => {
                 className="w-6 h-6"
               />
             </TouchableOpacity>
+
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user.avatar }}
+                resizeMode="cover"
+                className="w-14 h-14 rounded-lg"
+              />
+            </View>
+
+            <InfoBox
+              title={user.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+
+            <View className="mt-5 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-xl"
+              />
+
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
+            </View>
           </View>
         )}
         ListEmptyComponent={() => (
