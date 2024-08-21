8f20f6dd299f1e9cd12762051a4c940ff6df411d
Moazam05 <salmanmoazam08@gmail.com>
2024-08-21 19:59:45 +0500

    getUserPosts api function

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,11 +1,45 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../../components/EmptyState";
+import useAppWrite from "../../lib/useAppWrite";
+import VideoCard from "../../views/Home/components/VideoCard";
+import { getUserPosts } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { icons } from "../../constants";
 
 const Profile = () => {
+  const { user, setUser, setIsLogged } = useGlobalContext();
+
+  const { data: posts } = useAppWrite(() => getUserPosts(user.$id));
+
+  const logout = () => {};
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity className="w-full items-end" onPress={logout}>
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for the search query"
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
